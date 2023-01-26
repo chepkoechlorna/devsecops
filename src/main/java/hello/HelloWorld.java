3a1eb3f08fac0ba8a95e8384976f1679b3c25a93
chepkoechlorna <56117404+chepkoechlorna@users.noreply.github.com>
2023-01-26 13:23:13 +0300

    Update HelloWorld.java

--- src/main/java/hello/HelloWorld.java
+++ src/main/java/hello/HelloWorld.java
@@ -1,17 +1,10 @@
 package hello;
-
-import static org.hamcrest.CoreMatchers.containsString;
-import static org.junit.Assert.*;
-
-import org.junit.Test;
-
-public class Greeter {
-  
-  private Greeter greeter = new Greeter();
-
-  @Test
-  public void greeterSaysHello() {
-    assertThat(greeter.sayHello(), containsString("Hello"));
+import org.joda.time.LocalTime;
+public class HelloWorld {
+  public static void main(String[] args) {
+    LocalTime currentTime = new LocalTime();
+    System.out.println("The current local time is: " + currentTime);
+    Greeter greeter = new Greeter();
+    System.out.println(greeter.sayHello());
   }
-
 }
